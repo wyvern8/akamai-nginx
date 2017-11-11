9410c01dc3545ad3ec8d5b9f15f68d686203b5d0
wyvern8 <wyvern@smartcreations.com.au>
2017-11-11 17:11:05 +1100

    style(lint):

--- src/behaviors/downstreamCache.js
+++ src/behaviors/downstreamCache.js
@@ -13,56 +13,46 @@ export class BehaviorDownstreamCache extends Behavior {
         let expires = this.expires();
 
         let lua = this.switchByVal({
-            'ALLOW':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", "max-age=" .. ' + expires + '}',
-                    'aka_downstream_headers["Expires"] = {"SET", expiryDateString(' + expires + ')}'
-                ],
-            'MUST_REVALIDATE':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", "no-cache"}'
-                ],
-            'BUST':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", "max-age=0, no-cache, no-store"}',
-                    'aka_downstream_headers["Pragma"] = {"SET", "no-cache"}',
-                    'aka_downstream_headers["Expires"] = {"SET", "1 January, 1970 00:00:01 GMT"}'
-                ],
-            'TUNNEL_ORIGIN':
-                [
-                    '-- let cache headers pass though from origin'
-                ],
-            'NONE':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", nil}',
-                    'aka_downstream_headers["Expires"] = {"SET", nil}',
-                    'aka_downstream_headers["Pragma"] = {"SET", nil}'
-                ]
+            'ALLOW': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", "max-age=" .. ' + expires + '}',
+                'aka_downstream_headers["Expires"] = {"SET", expiryDateString(' + expires + ')}'
+            ],
+            'MUST_REVALIDATE': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", "no-cache"}'
+            ],
+            'BUST': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", "max-age=0, no-cache, no-store"}',
+                'aka_downstream_headers["Pragma"] = {"SET", "no-cache"}',
+                'aka_downstream_headers["Expires"] = {"SET", "1 January, 1970 00:00:01 GMT"}'
+            ],
+            'TUNNEL_ORIGIN': [
+                '-- let cache headers pass though from origin'
+            ],
+            'NONE': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", nil}',
+                'aka_downstream_headers["Expires"] = {"SET", nil}',
+                'aka_downstream_headers["Pragma"] = {"SET", nil}'
+            ]
         }, [], this.options.behavior);
 
         lua = [...lua, ...this.switchByVal({
-            'PASS_ORIGIN':
-                [
-                    '-- let cache headers pass though'
-                ],
-            'CACHE_CONTROL':
-                [
-                    'aka_downstream_headers["Expires"] = {"SET", nil}'
-                ],
-            'EXPIRES':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", nil}'
-                ],
-            'CACHE_CONTROL_AND_EXPIRES':
-                [
-                    '-- let cache headers pass though'
-                ],
-            'NONE':
-                [
-                    'aka_downstream_headers["Cache-Control"] = {"SET", nil}',
-                    'aka_downstream_headers["Expires"] = {"SET", nil}',
-                    'aka_downstream_headers["Pragma"] = {"SET", nil}'
-                ]
+            'PASS_ORIGIN': [
+                '-- let cache headers pass though'
+            ],
+            'CACHE_CONTROL': [
+                'aka_downstream_headers["Expires"] = {"SET", nil}'
+            ],
+            'EXPIRES': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", nil}'
+            ],
+            'CACHE_CONTROL_AND_EXPIRES': [
+                '-- let cache headers pass though'
+            ],
+            'NONE': [
+                'aka_downstream_headers["Cache-Control"] = {"SET", nil}',
+                'aka_downstream_headers["Expires"] = {"SET", nil}',
+                'aka_downstream_headers["Pragma"] = {"SET", nil}'
+            ]
         }, [], this.options.sendHeaders)];
 
 
